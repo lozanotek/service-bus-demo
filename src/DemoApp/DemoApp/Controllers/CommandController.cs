9db81368691f081d927e153c9408d568efecefbd
Javier Lozano <javier@lozanotek.com>
2016-04-18 12:09:25 -0500

    Initial commit

---
+++ src/DemoApp/DemoApp/Controllers/CommandController.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DemoMessages;
+using Microsoft.ServiceBus.Messaging;
+
+namespace DemoApp.Controllers
+{
+    public class CommandController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Index(string text)
+        {
+            var connectionString = ConfigurationManager.AppSettings["serviceBus.ConnectionString"];
+
+            var queueClient =
+              QueueClient.CreateFromConnectionString(connectionString, "demo");
+            
+            var message = new BrokeredMessage(new HelloCommand { Text = text } );
+            await queueClient.SendAsync(message);
+
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ src/DemoApp/DemoClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using DemoMessages;
+using Microsoft.ServiceBus.Messaging;
+
+namespace DemoClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionString = ConfigurationManager.AppSettings["serviceBus.ConnectionString"];
+            var queueClient =
+              QueueClient.CreateFromConnectionString(connectionString, "demo");
+
+            // Configure the callback options.
+            var options = new OnMessageOptions
+            {
+                AutoComplete = false,
+                AutoRenewTimeout = TimeSpan.FromMinutes(1),
+                MaxConcurrentCalls = 4
+            };
+
+            // Callback to handle received messages.
+            queueClient.OnMessage(message =>
+            {
+                try
+                {
+                    // Process message from queue.
+                    Console.WriteLine("Message ID: " + message.MessageId);
+                    Console.WriteLine("Body: " + message.GetBody<HelloCommand>());
+                    Console.WriteLine("Expires At (UTC): " + message.ExpiresAtUtc);
+
+                    // Remove message from queue.
+                    message.Complete();
+                }
+                catch (Exception)
+                {
+                    // Indicates a problem, unlock message in queue.
+                    message.Abandon();
+                }
+            }, options);
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
